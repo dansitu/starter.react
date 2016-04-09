3ac8474210509d3ba947a123e94b97466f40312b
Daniel Imrie-Situnayake <dansmailbag@googlemail.com>
2016-04-09 10:51:48 -0700

    Update app.jsx

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 ReactDOM.render(
-  <h1>Hello, wzrld!</h1>,
+  <h1>Hello, world!</h1>,
   document.getElementById('example')
 );
 
